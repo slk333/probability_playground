3bd8faac69e5e26837c801f6cf01e8e07bedfd8a
Antoine Weber <aweb33@icloud.com>
2019-03-09 15:11:26 +0100

    refactor clean

--- probability.playground/Contents.swift
+++ probability.playground/Contents.swift
@@ -1,51 +1,41 @@
-import UIKit
+import Foundation
 
 
-let pOfSucess = 0.20
-var pOfFailure = Double(1 - pOfSucess)
-let rolls = 10
 
-let p1 = pOfSucess
-let p2 = (1-pOfSucess) * pOfSucess
-let p3 = (1-pOfSucess) * (1-pOfSucess) * pOfSucess
-let p4 = pow((1-pOfSucess), 3) * pOfSucess
 
+// being the first success means being a success AND all the previous attemps are failures
 func pFirstSuccessAt(rollNumber:Int)->Double{
-    return pow((1-pOfSucess),Double(rollNumber-1)) * pOfSucess
+    return pSuccess * pow((1-pSuccess),Double(rollNumber-1))
+    
+    // let pFirstSuccessAtN = p(success at n) * p(failure of all previous)
+    // let pFirstSuccessAt1 = pOfSucess
+    // let pFirstSuccessAt2 = pOfSucess * pOfFailureOf1                  = pOfSucess * (1-pOfSucess)
+    // let pFirstSuccessAt3 = pOfSucess *  pOfFailureOf1 * pOfFailureOf2 = pOfSucess * (1-pOfSucess) * (1-pOfSucess)
+    // let pFirstSuccessAt4 =                                            = pOfSucess * pow((1-pOfSucess), 3)
 }
 
+
+// at least one success means adding the probability of all the first success
 func pAtLeastOneSuccessInN(rolls:Int)->Double{
-    var pOfSuccess = 0.0
+    var pAtLeastOneSuccess = 0.0
     for i in 1...rolls{
-        pOfSuccess += pFirstSuccessAt(rollNumber: i)
+        pAtLeastOneSuccess += pFirstSuccessAt(rollNumber: i)
     }
-    return pOfSuccess
+    return pAtLeastOneSuccess
 }
 
-let pAtLeastOneSucess = pAtLeastOneSuccessInN(rolls: 9)
-let pNoAnySucess = 1 - pAtLeastOneSucess
-1.0 / pNoAnySucess
+
+
+
+// using the functions
+let pSuccess = 0.20
+let pAtLeastOneSuccess = pAtLeastOneSuccessInN(rolls: 5)
+let pFailure = 1 - pAtLeastOneSuccess
+
+let NumberOfSuccessFor1Failure = 1 / pFailure
 
 
 
 
 
-
-var p : Double = 0.5 + 0.5 * 0.5 + 0.5 * 0.5 * 0.5
-
-var x = Decimal(0.0)
-for i in 1...10{
-    x += pow(0.5, i)
-}
-
-x
-
-
-// on lance un dé à 10 face 3 fois. Quelle est la probabilité d'avoir 1 au moins 1 fois sur 3 lancers. pour chaque lançer la chance est 0.1. on effectuer un lancer seulement si tous les précédants ont échoués. La chance pour le précédent ait échoué est 0.9. La chance pour que les deux premiers échouent est 0.9*0.9. p(1) p(2) p(3).
-
-// p(1) = 0.1
-// p(2 only) = not p(1) AND p(2) = (1 - p(1)) * p(2) = (1 - 0.1) * 0.1 = 0.9 * 0.1 = 0.09
-// p(3) = not p(1) AND not p(2) AND p(3) = 0.9*0.91 * 0.1 =    0.081
-// Z = 0.1 + 0.09 + 0.081 = 0.271
-// 72,9% of times, there won't be any 1 in 3 rolls
 
